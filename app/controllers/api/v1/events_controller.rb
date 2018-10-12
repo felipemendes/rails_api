06eb46b53ecae6e87dc9d40e9ece6025caa95636
felipemendes <f.ribeiromendes@gmail.com>
2018-10-11 22:48:40 -0300

    Add create and update endpoints

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,8 +1,25 @@
 module Api::V1
   class EventsController < ApplicationController
     def index
-      @events = Event.all
+      @events = Event.order("created_at DESC")
       render json: @events
+    end
+
+    def create
+      @event = Event.create(event_params)
+      render json: @event
+    end
+
+    def update
+      @event = Event.find(params[:id])
+      @event.update_attributes(event_params)
+      render json: @idea
+    end
+
+    private
+
+    def event_params
+      params.require(:event).permit(:title, :body)
     end
   end
 end
